7be0d7dbae0cb024361fd725323aca65d070b3b3
naufalw <naufalwiwitputra@gmail.com>
2021-09-05 22:37:17 +0700

    lava: dotOS bringup

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -15,9 +15,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-   $(LOCAL_DIR)/lineage_lava.mk
+   $(LOCAL_DIR)/dot_lava.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_lava-user \
-    lineage_lava-userdebug \
-    lineage_lava-eng
+    dot_lava-user \
+    dot_lava-userdebug \
+    dot_lava-eng
